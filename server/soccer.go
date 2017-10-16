57d55086b062611e9af175b088464d73c9481dad
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2017-10-16 19:43:21 +0200

    Better organisation for teams requests

--- server/soccer.go
+++ server/soccer.go
@@ -1,64 +1,18 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"net/http"
 
 	"github.com/tsauvajon/ws-blockchain/server/sportmonks"
 )
 
 func (app *App) getTeam(w http.ResponseWriter, r *http.Request) {
-	client := &http.Client{}
-
-	url := fmt.Sprintf("https://soccer.sportmonks.com/api/v2.0/teams/53?api_token=QCaGhUsahBbiKEQPGINe7G843Jr5qSsdKmPpUpvR4MJee7xGYw4t63z4YgkO")
-
-	// Prepare the request
-	req, err := http.NewRequest("GET", url, nil)
+	team, err := sportmonks.GetTeam(53)
 
 	if err != nil {
-		fmt.Println("Error creating the team: ", err)
+		respondWithJSON(w, 500, err)
 		return
 	}
-
-	// Execute the request
-	res, err := client.Do(req)
-
-	if err != nil {
-		fmt.Println("Error executing the GetTeam request: ", err)
-		return
-	}
-
-	defer res.Body.Close()
-
-	var response sportmonks.Response
-
-	// Read the response
-	body, err := ioutil.ReadAll(res.Body)
-
-	if err != nil {
-		fmt.Println("Error reading the response body", err)
-	}
-
-	// Unmarshal the response : { data: { ... }, meta: { ... }}
-	if err = json.Unmarshal(body, &response); err != nil {
-		fmt.Println("Error unmarshalling the response", err)
-	}
-
-	var team sportmonks.Team
-
-	jsonEncodedTeam, err := json.Marshal(response.Data)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	if err = json.Unmarshal(jsonEncodedTeam, &team); err != nil {
-		fmt.Println("Error unmarshalling the response data", err)
-	}
-
-	fmt.Println(team)
 
 	respondWithJSON(w, 200, team)
 }
--- server/sportmonks/teams.go
+++ server/sportmonks/teams.go
@@ -1,5 +1,13 @@
 package sportmonks
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+)
+
 // Team : équipe
 type Team struct {
 	// ID of team
@@ -20,4 +28,56 @@ type Team struct {
 	LogoPath string `json:"logo_path"`
 	// VenueID : match ID
 	VenueID int `json:"venue_id"`
+}
+
+// GetTeam : retrieves a team from the sportmonks API
+func GetTeam(teamID int) (Team, error) {
+	client := &http.Client{}
+
+	url := fmt.Sprintf("https://soccer.sportmonks.com/api/v2.0/teams/" + strconv.Itoa(teamID) + "?api_token=QCaGhUsahBbiKEQPGINe7G843Jr5qSsdKmPpUpvR4MJee7xGYw4t63z4YgkO")
+
+	// Prepare the request
+	req, err := http.NewRequest("GET", url, nil)
+
+	if err != nil {
+		return Team{}, fmt.Errorf("Error creating the team: %v", err)
+	}
+
+	// Execute the request
+	res, err := client.Do(req)
+
+	if err != nil {
+		return Team{}, fmt.Errorf("Error executing the GetTeam request: %v", err)
+	}
+
+	defer res.Body.Close()
+
+	var response Response
+
+	// Read the response
+	body, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		return Team{}, fmt.Errorf("Error reading the response body: %v", err)
+	}
+
+	// Unmarshal the response : { data: { ... }, meta: { ... }}
+	if err = json.Unmarshal(body, &response); err != nil {
+		return Team{}, fmt.Errorf("Error unmarshalling the response: %v", err)
+	}
+
+	var team Team
+
+	// Marshal the data part in order to decode it from JSON later
+	jsonEncodedTeam, err := json.Marshal(response.Data)
+
+	if err != nil {
+		return Team{}, fmt.Errorf("Error marshalling the team: %v", err)
+	}
+
+	if err = json.Unmarshal(jsonEncodedTeam, &team); err != nil {
+		return Team{}, fmt.Errorf("Error unmarshalling the response data: %v", err)
+	}
+
+	return team, nil
 }
