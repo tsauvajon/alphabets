2e3c23598d9e47193159b4243ede29f7cde61cac
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2017-10-16 19:51:32 +0200

    Retrieve any team: api/soccer/teams/{id}

--- server/soccer.go
+++ server/soccer.go
@@ -2,15 +2,29 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/tsauvajon/ws-blockchain/server/sportmonks"
 )
 
 func (app *App) getTeam(w http.ResponseWriter, r *http.Request) {
-	team, err := sportmonks.GetTeam(53)
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithJSON(w, 400, "Invalid team ID")
+		return
+	}
+
+	team, err := sportmonks.GetTeam(id)
 
 	if err != nil {
 		respondWithJSON(w, 500, err)
+		return
+	}
+
+	if team.ID == 0 {
+		respondWithJSON(w, 404, "Team not found")
 		return
 	}
 
